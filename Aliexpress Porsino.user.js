1a50936496ca4c7e4ed5e9b437db877e3b4aedaa
brcrepo <el.info.mail@gmail.com>
2016-09-08 00:29:19 -0300

    se cambio el link del json y se agrego nuevo destacado
    se cambia el link del json a github y ahora las tiendas que muestren envío por via AliExpress Standard Shipping serán destacadas en amarillo

--- Aliexpress Porsino.user.js
+++ Aliexpress Porsino.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Aliexpress Porsino
 // @namespace    chw.orochimaru
-// @version      0.1
+// @version      0.2
 // @description  Script para destacar tiendas que hacen envíos por sinotrans en aliexpress
 // @author       BRC
 // @match        *.aliexpress.com/*
@@ -9,9 +9,12 @@
 // @grant        none
 // ==/UserScript==
 
-var jsonPorSino = "https://cdn.rawgit.com/brcrepo/Aliexpress-por-sino/master/tiendasPorSino.json";
+//var jsonPorSino = "https://cdn.rawgit.com/brcrepo/Aliexpress-por-sino/master/tiendasPorSino.json";
+var jsonPorSino = "https://raw.githubusercontent.com/brcrepo/Aliexpress-por-sino/master/tiendasPorSino.json";
+
 var tiendasPorSino;
 
+//trae el json con el arreglo que contiene los numeros de tiendas
 $.getJSON(jsonPorSino ,function(data){
 
     tiendasPorSino = data.tiendasPorSino;
@@ -20,18 +23,32 @@ $.getJSON(jsonPorSino ,function(data){
 
 $(function(){
 
+    //listado de las tiendas que se muestran en la busqueda
     var listaDeProductos = $('li.list-item');
 
     $(listaDeProductos).each(function(){
 
+        //extrae el numero de tienda
         var tienda = $(this).find(".store ").attr("href").split('/').pop();
+
+        //ve si la tienda muestra envio por AliExpress Standard Shipping
+        var isViaASS = $(this).find("dd.price").text().match(/via AliExpress Standard Shipping/gi);
+
+        //comprueba que el numero de tienda este en el arreglo de tiendas conocidas
         var isPorsino = $.inArray(tienda, tiendasPorSino);
 
         if(isPorsino !== -1){
 
-            $(this).css("background-color","#8DCF8A");
+            //si la tienda es conocida le cambia el color de fondo a verde en la lista de productos
+            $(this).css("background-color","#6B8E23");
+
+        }else if(isViaASS){
+
+            //si la tienda muestra envio por AliExpress Standard Shipping le cambia el color de fondo a amarillo en la lista de productos
+            $(this).css("background-color","#ffe700");
 
         }
+
 
     });
 
