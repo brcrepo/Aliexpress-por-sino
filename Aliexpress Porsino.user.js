557f07dddc4e54ab83419a19e91e4855bb2427b9
brcrepo <el.info.mail@gmail.com>
2016-11-19 22:22:32 -0300

    versión 0.4
    agregado coloreo para tiendas inconsistentes

--- Aliexpress Porsino.user.js
+++ Aliexpress Porsino.user.js
@@ -1,27 +1,29 @@
 // ==UserScript==
 // @name         Aliexpress Porsino
 // @namespace    chw.orochimaru
-// @version      0.3
+// @version      0.4
 // @description  Script para destacar tiendas que hacen envíos por sinotrans en aliexpress
 // @author       BRC
 // @match        *.aliexpress.com/*
 // @require      http://code.jquery.com/jquery-latest.js
 // @grant        none
 // ==/UserScript==
-
-//var jsonPorSino = "https://cdn.rawgit.com/brcrepo/Aliexpress-por-sino/master/tiendasPorSino.json";
 var jsonPorSino = "https://raw.githubusercontent.com/brcrepo/Aliexpress-por-sino/master/tiendasPorSino.json";
+var jsonTiendasBorradas = "https://raw.githubusercontent.com/brcrepo/Aliexpress-por-sino/master/tiendasBorradas.json";
 
 var tiendasPorSino;
+var tiendasBorradas;
 
 //trae el json con el arreglo que contiene los numeros de tiendas
 $.getJSON(jsonPorSino ,function(data){
-
     tiendasPorSino = data.tiendasPorSino;
-
 });
 
-$(function(){
+$.getJSON(jsonTiendasBorradas ,function(data){
+    tiendasBorradas = data.tiendasBorradas;
+});
+
+function colorear(){
 
     //listado de las tiendas que se muestran en la busqueda
     var listaDeProductos = $('li.list-item');
@@ -29,7 +31,8 @@ $(function(){
     $(listaDeProductos).each(function(){
 
         //extrae el numero de tienda
-        var tienda = $(this).find(".store ").attr("href").split('/').pop();
+        var tienda = $(this).find("a.store ").attr("href").split('/').pop();
+        //console.log(tienda);
 
         //ve si la tienda muestra envio por AliExpress Standard Shipping
         var isViaASS = $(this).find("dd.price").text().match(/via AliExpress Standard Shipping/gi);
@@ -37,10 +40,25 @@ $(function(){
         //comprueba que el numero de tienda este en el arreglo de tiendas conocidas
         var isPorsino = $.inArray(tienda, tiendasPorSino);
 
+        var isBorrada = $.inArray(tienda, tiendasBorradas);
+
         if(isPorsino !== -1){
 
             //si la tienda es conocida le cambia el color de fondo a verde en la lista de productos
             $(this).css("background-color","#6B8E23");
+
+        }else if(isBorrada!== -1){
+
+            //tiendas que envian por sino a veces si y a veces no
+            $(this).css({'background': '#60b807',
+                         'background':'-moz-linear-gradient(-45deg, #60b807 0%, #efff08 50%)',
+                         'background':' -webkit-gradient(left top, right bottom, color-stop(0%, #60b807), color-stop(50%, #efff08))',
+                         'background':' -webkit-linear-gradient(-45deg, #60b807 0%, #efff08 50%)',
+                         'background':' -o-linear-gradient(-45deg, #60b807 0%, #efff08 50%)',
+                         'background':' -ms-linear-gradient(-45deg, #60b807 0%, #efff08 50%)',
+                         'background':' linear-gradient(135deg, #60b807 0%, #efff08 50%)'});
+
+
 
         }else if(isViaASS){
 
@@ -52,7 +70,7 @@ $(function(){
 
     });
 
-});
+}
 
 function correosChileLink(){
 
@@ -67,4 +85,9 @@ function correosChileLink(){
     }
 }
 
-correosChileLink();
+$(function(){
+
+    colorear();
+    correosChileLink();
+
+});
